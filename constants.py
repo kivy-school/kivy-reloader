56e9b09c0a00682e574923c5b86b607b435d428f
Filipe Marchesini <filipe.marchesini@gmail.com>
2023-10-31 15:58:57 -0300

    🔧 Updated `constants.py`: Added: - WATCHED_KV_FOLDERS_RECURSIVELY - WATCHED_KV_FOLDERS
    Added more comments to make clear the usage of each constant

--- constants.py
+++ constants.py
@@ -4,27 +4,37 @@ PHONE_IPS = ["192.168.1.65"]
 
 
 # You can stream using "USB" or "WIFI"
+# If your USB cable is connected, use "USB", otherwise use "WIFI"
+# To use WIFI, you need connect your phone to the same network as your computer
 STREAM_USING = "USB"
 
 
 # The port where the server will be listening
+# It can be any port
 PORT = 5555
 
 
-# Add the files you want to watch here, i.e., if they change, the app will be reloaded
+# Add the python files you want to watch here, i.e., if they change, the app will be reloaded
 WATCHED_FILES = ["main.py"]
 
 
-# Add the folders you want to watch recursively here, i.e., if any file inside them changes, the app will be reloaded
+# Add the folders you want to watch recursively here, i.e., if **ANY FILE** inside them changes, the app will be reloaded
 WATCHED_FOLDERS_RECURSIVELY = ["screens"]
 
 
-# Watched folders but not recursively, i.e., just the files inside the first level of the folder
+# Watched folders but not recursively, i.e., **ANY FILE** inside the first level of the folder
 WATCHED_FOLDERS = []
+
+# Add the folders where you have your .kv files here
+# If you don't put them here, the .kv files won't be reloaded
+# this is recursive, i.e., it will watch all folders inside the folders you put here
+WATCHED_KV_FOLDERS_RECURSIVELY = ["screens"]
 
 
 # Add the folders where you have your .kv files here
-KV_FILES_FOLDERS = ["screens"]
+# If you don't put them here, the .kv files won't be reloaded
+# this is not recursive, i.e., it will only watch the first level of the folders you put here
+WATCHED_KV_FOLDERS = []
 
 
 # If you want to exclude some files or folders, from being copied to the phone, add them here
