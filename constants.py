2ddab6a5d21309e4b48ab0b0ddfa8695d39adccf
Filipe Marchesini <filipe.marchesini@gmail.com>
2023-10-31 14:26:11 -0300

    ♻️ refactor: Making the `constants.py` easier to read

--- constants.py
+++ constants.py
@@ -1,17 +1,33 @@
-PHONE_IPS = ["192.168.100.200"]  # The IP address of your phone
-STREAM_USING = "USB"  # "USB" or "WIFI"
+# The IP address of your phone; You can hot-reload how many phones you want.
+# use "127.0.0.1" if you are using an emulator.
+PHONE_IPS = ["192.168.1.65"]
+
+
+# You can stream using "USB" or "WIFI"
+STREAM_USING = "USB"
+
+
+# The port where the server will be listening
 PORT = 5555
 
-WATCHED_FILES = [
-    "main.py"
-]  # Add the files you want to watch here, i.e., if they change, the app will be reloaded
-WATCHED_FOLDERS_RECURSIVELY = [
-    "screens"
-]  # Add the folders you want to watch here, i.e., if any file inside them changes, the app will be reloaded
-WATCHED_FOLDERS = []  # Watched folders but not recursively
 
-KV_FILES_FOLDERS = ["screens"]  # Add the folders where you have your .kv files here
+# Add the files you want to watch here, i.e., if they change, the app will be reloaded
+WATCHED_FILES = ["main.py"]
 
+
+# Add the folders you want to watch recursively here, i.e., if any file inside them changes, the app will be reloaded
+WATCHED_FOLDERS_RECURSIVELY = ["screens"]
+
+
+# Watched folders but not recursively, i.e., just the files inside the first level of the folder
+WATCHED_FOLDERS = []
+
+
+# Add the folders where you have your .kv files here
+KV_FILES_FOLDERS = ["screens"]
+
+
+# If you want to exclude some files or folders, from being copied to the phone, add them here
 FOLDERS_AND_FILES_TO_EXCLUDE_FROM_PHONE = [
     "*.pyc",
     "__pycache__",
