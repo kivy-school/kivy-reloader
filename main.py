d7eaad5f16dfc1301700c5f4aad8185a650ea63f
Filipe Marchesini <filipe.marchesini@gmail.com>
2023-12-07 04:32:03 -0300

    🐛 fix #3: I was importing the python file on the wrong place
    We need to import the screens inside the `build_and_reloader` function

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from kivy_reloader import App
-from screens.main_screen import MainScreen
 
 
 class MainApp(App):
     should_send_app_to_phone = False
 
     def build_and_reload(self):
+        from screens.main_screen import MainScreen
+
         self.screen_manager.add_widget(MainScreen())
         return self.reloader
 
