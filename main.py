5d8eb0cc14b026d372ac77078bca8d525d84cc85
Filipe Marchesini <filipe.marchesini@gmail.com>
2023-12-13 02:59:26 -0300

    ♻️ refactor main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from kivy.app import runTouchApp
-from kivy.uix.button import Button
-
 from kivy_reloader import App
 
 
@@ -8,17 +5,9 @@ class MainApp(App):
     should_send_app_to_phone = True
 
     def build_and_reload(self, initialize_server=None, *args):
-        print("a")
         from screens.main_screen import MainScreen
 
-        main_screen = MainScreen(name="Main Screen")
-        if "Main Screen" not in self.screen_manager.screen_names:
-            print("ADDING MAIN SCREEN")
-            self.screen_manager.add_widget(main_screen)
-        return self.reloader
+        return MainScreen(name="Main Screen")
 
 
 MainApp()
-
-
-# runTouchApp(Button(text="Hello World"))
