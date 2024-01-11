50d051cf55be26e3d6ae393fe1930a866bdebec3
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-01-11 02:36:23 -0300

    ♻️ Refactor compilation and debugging process, added new constants

--- kivy_reloader/compile_app.py
+++ kivy_reloader/compile_app.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import subprocess
 import time
 from multiprocessing import Process
 from threading import Thread
@@ -8,144 +10,212 @@ from colorama import Fore, init
 from icecream import ic
 from kivy.utils import platform
 
-from .constants import PHONE_IPS, PORT, STREAM_USING
+from .utils import (
+    ALWAYS_ON_TOP,
+    PHONE_IPS,
+    PORT,
+    SHOW_TOUCHES,
+    STAY_AWAKE,
+    STREAM_USING,
+    TURN_SCREEN_OFF,
+    WINDOW_TITLE,
+    WINDOW_WIDTH,
+    WINDOW_X,
+    WINDOW_Y,
+)
 
 if platform in ["linux", "macosx"]:
     from plyer import notification
 
-red = Fore.RED
-green = Fore.GREEN
 yellow = Fore.YELLOW
 init(autoreset=True)
 
-app_name = (
-    [
-        line
-        for line in open("buildozer.spec", "r").readlines()
-        if line.startswith("title")
-    ][0]
-    .split("=")[1]
-    .strip()
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 
-class Compilation:
-    @classmethod
-    def start(cls, option):
-        """
-        Starts the compilation process
-        """
+def notify(title: str, message: str) -> None:
+    """
+    Send a notification to the user's desktop.
+    No support for Windows yet.
+    """
+    if platform in ["linux", "macosx"]:
+        notification.notify(message=message, title=title)
+
+
+def start_compilation(option: int, app_name: str) -> None:
+    """
+    Starts the compilation process based on the given option.
+    """
+    try:
         if option == "1":
-            print(f"{yellow} Starting compilation")
+            logging.info("Starting compilation")
+            notify(
+                f"Compiling {app_name}",
+                f"Compilation started at {time.strftime('%H:%M:%S')}",
+            )
             t1 = time.time()
-            cls.notify_that_compilation_started()
-            os.system("buildozer -v android debug deploy run")
+            subprocess.run(
+                ["buildozer", "-v", "android", "debug", "deploy", "run"], check=True
+            )
             t2 = time.time()
-            cls.notify_that_compilation_finished(t2 - t1)
-            print(f"{green} Finished compilation")
-            cls.debug_and_livestream()
-        else:
-            cls.debug_and_livestream()
-
-    def notify_that_compilation_started():
-        """
-        Notifies the user that the compilation started
-        """
-        if platform in ["linux", "macosx"]:
-            notification.notify(
-                message=f"Compilation started at {time.strftime('%H:%M:%S')}",
-                title=f"Compiling {app_name}",
+            notify(
+                f"Compiled {app_name} successfully",
+                f"Compilation finished in {round(t2 - t1, 2)} seconds",
             )
-
-    def notify_that_compilation_finished(time):
-        """
-        Notifies the user that the compilation finished
-        """
-        if platform in ["linux", "macosx"]:
-            notification.notify(
-                message=f"Compilation finished in {round(time, 2)} seconds",
-                title=f"Compiled {app_name}",
-            )
-
-    @classmethod
-    def debug_and_livestream(cls):
-        proc = Process(target=cls.debug)
-        proc2 = Process(target=cls.livestream)
-        proc.start()
-        proc2.start()
-        while True:
-            try:
-                time.sleep(1)
-            except KeyboardInterrupt:
-                proc.terminate()
-                proc2.terminate()
-                break
-
-    @classmethod
-    def debug_on_usb(cls):
-        ic()
-        os.system("adb logcat | grep 'I python'")
-
-    @classmethod
-    def debug_on_wifi(cls):
-        ic()
-        os.system(f"adb tcpip {PORT}")
-
-        def start_logcat(IP, PORT):
-            print(f"Connecting to {IP}")
-            os.system(f"adb connect {IP}")
-            print("Connected")
-
-            print("Starting logcat")
-            os.system(f"adb -s {IP}:{PORT} logcat | grep 'I python'")
-            print("Finished logcat")
-
-        for IP in PHONE_IPS:
-            # start each logcat on a thread
-            t = Thread(target=start_logcat, args=(IP, PORT))
-            t.start()
-
-    @classmethod
-    def debug(cls):
-        os.system("adb disconnect")
-        os.system("adb kill-server")
-        os.system("adb start-server")
-
-        print("Clearing logcat")
-        os.system("adb logcat -c")
-        print("Logcat cleared")
-
-        if STREAM_USING == "USB":
-            cls.debug_on_usb()
-            return
-        elif STREAM_USING == "WIFI":
-            cls.debug_on_wifi()
-            return
-
-    def livestream():
-        if STREAM_USING == "WIFI":
-            time.sleep(3)
-
-        for proc in psutil.process_iter():
-            try:
-                if proc.name() == "scrcpy":
-                    print("yes")
-                    break
-            except psutil.NoSuchProcess:
-                print("error")
+            logging.info("Finished compilation")
+            debug_and_livestream()
         else:
-            print("Starting scrcpy")
-            command = "scrcpy --window-x 1200 --window-y 100 --window-width 280 --always-on-top"
-            if STREAM_USING == "USB":
-                os.system(f"{command} -d")
-            elif STREAM_USING == "WIFI":
-                os.system(f"{command} -e")
+            debug_and_livestream()
+    except subprocess.CalledProcessError as e:
+        logging.error(f"An error occurred during compilation: {e}")
+
+
+def debug_and_livestream() -> None:
+    """
+    Executes `adb logcat` and `scrcpy` in parallel.
+    """
+    try:
+        adb_logcat = Process(target=debug)
+        scrcpy = Process(target=livestream)
+
+        adb_logcat.start()
+        scrcpy.start()
+    except KeyboardInterrupt:
+        logging.info("Terminating processes")
+        adb_logcat.terminate()
+        scrcpy.terminate()
+
+
+def debug():
+    """
+    Debugging based on the streaming method.
+    """
+    if STREAM_USING == "USB":
+        restart_adb_server()
+        clear_logcat()
+        run_logcat()
+    elif STREAM_USING == "WIFI":
+        debug_on_wifi()
+
+
+def restart_adb_server():
+    """
+    Restarts the adb server.
+    """
+    logging.info("Restarting adb server")
+    subprocess.run(["adb", "disconnect"])
+    subprocess.run(["adb", "kill-server"])
+    subprocess.run(["adb", "start-server"])
+
+
+def clear_logcat():
+    """
+    Clears the logcat.
+    """
+    logging.info("Clearing logcat")
+    subprocess.run(["adb", "logcat", "-c"])
+
+
+def debug_on_wifi():
+    """
+    Debugging over WiFi.
+    """
+    subprocess.run(["adb", "tcpip", f"{PORT}"])
+
+    for IP in PHONE_IPS:
+        # start each logcat on a thread
+        t = Thread(target=run_logcat, args=(IP,))
+        t.start()
+
+
+def run_logcat(IP=None, *args):
+    """
+    Runs logcat for debugging.
+    """
+    logcat_command = "adb logcat | grep 'I python'"
+
+    if IP:
+        subprocess.run(["adb", "connect", f"{IP}"])
+        logcat_command.replace("adb", f"adb -s {IP}:{PORT}")
+
+    logging.info("Starting logcat")
+    subprocess.run(logcat_command, shell=True)
+
+
+def livestream():
+    """
+    Handles the livestream process.
+    """
+    if STREAM_USING == "WIFI":
+        time.sleep(3)
+
+    for proc in psutil.process_iter():
+        try:
+            if proc.name() == "scrcpy":
+                logging.info("scrcpy already running")
+                return
+        except psutil.NoSuchProcess:
+            logging.error("Error while trying to find scrcpy process")
+
+    start_scrcpy()
+
+
+def start_scrcpy():
+    """
+    Starts the scrcpy process for screen mirroring.
+    """
+    logging.info("Starting scrcpy")
+    command = [
+        "scrcpy",
+        "--window-x",
+        WINDOW_X,
+        "--window-y",
+        WINDOW_Y,
+        "--window-width",
+        WINDOW_WIDTH,
+    ]
+
+    if STREAM_USING == "USB":
+        if ALWAYS_ON_TOP:
+            command.append("--always-on-top")
+        if TURN_SCREEN_OFF:
+            command.append("--turn-screen-off")
+        if STAY_AWAKE:
+            command.append("--stay-awake")
+        if SHOW_TOUCHES:
+            command.append("--show-touches")
+        if WINDOW_TITLE:
+            command.append("--window-title")
+            command.append(WINDOW_TITLE)
+        command.append("-d")
+    elif STREAM_USING == "WIFI":
+        command.append("-e")
+
+    subprocess.run(command)
+
+
+def get_app_name():
+    """
+    Extracts the application name from the 'buildozer.spec' file.
+    """
+    with open("buildozer.spec", "r") as file:
+        for line in file:
+            if line.startswith("title"):
+                return line.split("=")[1].strip()
+    return "UnknownApp"
 
 
 def start():
+    """
+    Entry point for the script. Prompts the user to choose an option.
+    """
     print(f"{yellow} Choose an option:")
+    app_name = get_app_name()
     option = input("1 - Compile, debug and livestream\n2 - Debug and livestream\n")
-    Compilation.start(option)
+    start_compilation(option, app_name)
 
 
 if __name__ == "__main__":
