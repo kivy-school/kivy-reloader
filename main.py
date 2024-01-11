6ee9d0555645835f3772a70e443e49e518ae13e5
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-01-11 03:21:15 -0300

    ♻️ refactor build_and_reload method in MainApp class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy_reloader import App
 
 
 class MainApp(App):
-    def build_and_reload(self, initialize_server=False, *args):
+    def build_and_reload(self):
         from screens.main_screen import MainScreen
 
         return MainScreen(name="Main Screen")
