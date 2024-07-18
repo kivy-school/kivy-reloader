24355b6e582eaf4ee2e50eebb9c2fc0892840d59
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-07-18 20:21:45 -0300

    🚚 Moved `screens` folder to `beautifulapp` folder

--- beautifulapp/screens/main_screen.py
+++ beautifulapp/screens/main_screen.py
@@ -2,7 +2,7 @@ from kivy.factory import Factory as F
 
 from kivy_reloader.utils import load_kv_path
 
-load_kv_path("screens/main_screen.kv")
+load_kv_path("beautifulapp/screens/main_screen.kv")
 
 
 class MainScreen(F.Screen):
