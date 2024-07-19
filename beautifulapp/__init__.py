4b2abbdff126e3b9c66fd7549eeb3ba19ab9b703
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-07-19 16:37:52 -0300

    ♻️ refactor: Added extra line so that the app works the same way with or without reloader

--- beautifulapp/__init__.py
+++ beautifulapp/__init__.py
@@ -2,6 +2,9 @@ from kivy_reloader.app import App
 
 
 class MainApp(App):
+    def build(self):
+        return self.build_and_reload()
+
     def build_and_reload(self):
         from .screens.main_screen import MainScreen
 
