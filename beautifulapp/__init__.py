f6bda64b7e82da416439844d53c5e56415a6efb1
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-07-19 20:48:07 -0300

    ⚗️ Experimenting having a unique `build` function

--- beautifulapp/__init__.py
+++ beautifulapp/__init__.py
@@ -1,14 +1,31 @@
+# from kivy_reloader.app import App
+
+
+# class MainApp(App):
+#     def build(self):
+#         #     return self.build_and_reload()
+
+#         # def build_and_reload(self):
+#         from .screens.main_screen import MainScreen
+
+#         return MainScreen(name="Main Screen")
+
+
+# app = MainApp()
 from kivy_reloader.app import App
 
 
 class MainApp(App):
     def build(self):
-        return self.build_and_reload()
+        print("executing build!!!")
+        from kivy.factory import Factory as F
 
-    def build_and_reload(self):
         from .screens.main_screen import MainScreen
 
-        return MainScreen(name="Main Screen")
+        screen_manager = F.ScreenManager()
+        screen_manager.add_widget(MainScreen(name="Main Screen"))
+
+        return screen_manager
 
 
 app = MainApp()
