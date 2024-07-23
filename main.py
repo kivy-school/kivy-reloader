0a5f2b3c8d6a23bdd5776a10c829111ebdf776f4
AccelQuasarDragon <138998466+AccelQuasarDragon@users.noreply.github.com>
2024-07-23 09:35:00 -0700

    fixed hidden code specific to beautiful app

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import trio
 
-from beautifulapp import app
+from kivy_reloader.app import App
+
+
+class MainApp(App):
+    def build(self):
+        from beautifulapp.screens.main_screen import MainScreen
+
+        return MainScreen(name="Main Screen")
+
+
+app = MainApp()
+
 
 trio.run(app.async_run, "trio")
