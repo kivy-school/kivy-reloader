989ee15ca9df4f2a08c8fa97b28bfe60df789259
filipemarch <filipe.marchesini@gmail.com>
2024-07-23 02:07:39 -0300

    ♻️ refactor: Using `os.path.join` to guarantee example works on on all OSs

--- beautifulapp/screens/main_screen.py
+++ beautifulapp/screens/main_screen.py
@@ -4,8 +4,8 @@ from kivy.factory import Factory as F
 
 from kivy_reloader.utils import load_kv_path
 
-load_kv_path(os.path.join("beautifulapp", "screens", "main_screen.kv"))
-
+main_screen_kv = os.path.join("beautifulapp", "screens", "main_screen.kv")
+load_kv_path(main_screen_kv)
 
 class MainScreen(F.Screen):
     def on_enter(self, *args):
