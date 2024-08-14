5a588f9e1f1299de365f0d976dacb6ae4fdd09cb
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-08-14 03:14:27 -0300

    ✨ feat: Updating example reloader to new structure
    Imports do not need to be inside `build`, they can happen on top of the file

--- beautifulapp/__init__.py
+++ beautifulapp/__init__.py
@@ -1,11 +1,7 @@
+from beautifulapp.screens.main_screen import MainScreen
 from kivy_reloader.app import App
 
 
 class MainApp(App):
     def build(self):
-        from .screens.main_screen import MainScreen
-
         return MainScreen(name="Main Screen")
-
-
-app = MainApp()
