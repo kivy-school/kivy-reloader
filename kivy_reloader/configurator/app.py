38d6da1975428565cdba38d39fc46788b8901573
filipemarch <filipe.marchesini@gmail.com>
2025-10-17 04:20:19 -0300

    🔧 fix: correct import paths for CoreScreen and load_theme in app.py

--- kivy_reloader/configurator/app.py
+++ kivy_reloader/configurator/app.py
@@ -1,8 +1,8 @@
-from configurator.screens.core import CoreScreen
-from configurator.theme import load_theme
 from kivy.core.window import Window
 
 from kivy_reloader.app import App
+from kivy_reloader.configurator.screens.core import CoreScreen
+from kivy_reloader.configurator.theme import load_theme
 
 load_theme()
 Window.size = (1100, 600)
