24d80460eed88b31540775be1c182f22bb94b78a
filipemarch <filipe.marchesini@gmail.com>
2025-10-22 01:54:42 -0300

    ✨ feat: add common reusable UI components to the widget module: Card, CustomSwitch, RadioGroup, ChipInput, TextField and BoxChipInput

---
+++ kivy_reloader/configurator/widgets/common/__init__.py
@@ -0,0 +1,18 @@
+"""Common reusable UI components"""
+
+from .box_chip_input import BoxChipInput
+from .card import Card
+from .chip_input import ChipInput
+from .radio_button import RadioButton, RadioGroup
+from .switch import CustomSwitch
+from .text_field import TextField
+
+__all__ = [
+    'Card',
+    'CustomSwitch',
+    'RadioGroup',
+    'RadioButton',
+    'ChipInput',
+    'TextField',
+    'BoxChipInput',
+]
