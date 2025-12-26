aa7def72de37dcf2dedb471e88fd39762e905557
filipemarch <filipe.marchesini@gmail.com>
2025-12-26 20:54:51 -0300

    ✨ feat: update __init__.py to include all card imports in the Cards package

--- kivy_reloader/configurator/widgets/cards/__init__.py
+++ kivy_reloader/configurator/widgets/cards/__init__.py
@@ -1,6 +1,25 @@
 """Cards package"""
 
+from .advanced_card import AdvancedCard
+from .audio_card import AudioCard
 from .core_card import CoreCard
+from .deployment_card import DeploymentCard
+from .device_card import DeviceCard
+from .display_card import DisplayCard
+from .notifications_card import NotificationsCard
+from .performance_card import PerformanceCard
 from .services_card import ServicesCard
+from .window_card import WindowCard
 
-__all__ = ['CoreCard', 'ServicesCard']
+__all__ = [
+    'AdvancedCard',
+    'AudioCard',
+    'CoreCard',
+    'DeploymentCard',
+    'DeviceCard',
+    'DisplayCard',
+    'NotificationsCard',
+    'PerformanceCard',
+    'ServicesCard',
+    'WindowCard',
+]
