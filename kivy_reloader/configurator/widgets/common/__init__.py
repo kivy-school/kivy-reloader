d440dff5e79087aed41ae2e5a75240b6b4e6e49b
filipemarch <filipe.marchesini@gmail.com>
2025-12-26 22:03:17 -0300

    ✨ feat: add CollapsibleSection and ConfirmPopup to common UI components

--- kivy_reloader/configurator/widgets/common/__init__.py
+++ kivy_reloader/configurator/widgets/common/__init__.py
@@ -3,12 +3,16 @@
 from .box_chip_input import BoxChipInput
 from .card import Card
 from .chip_input import ChipInput
+from .collapsible_section import CollapsibleSection
+from .confirm_popup import ConfirmPopup
 from .radio_button import RadioButton, RadioGroup
 from .switch import CustomSwitch
 from .text_field import TextField
 
 __all__ = [
     'Card',
+    'CollapsibleSection',
+    'ConfirmPopup',
     'CustomSwitch',
     'RadioGroup',
     'RadioButton',
