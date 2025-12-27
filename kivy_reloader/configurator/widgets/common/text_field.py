edc4696e627e757cf2f619e62bd2cad9af5489ca
filipemarch <filipe.marchesini@gmail.com>
2025-12-26 22:03:36 -0300

    ✨ feat: add error_text property for validation feedback in TextField

--- kivy_reloader/configurator/widgets/common/text_field.py
+++ kivy_reloader/configurator/widgets/common/text_field.py
@@ -1,5 +1,5 @@
 from kivy.metrics import dp
-from kivy.properties import ListProperty, ObjectProperty
+from kivy.properties import ListProperty, ObjectProperty, StringProperty
 from kivy.uix.textinput import TextInput
 
 from kivy_reloader.lang import Builder
@@ -11,4 +11,5 @@ class TextField(TextInput):
     radius: list | tuple = ListProperty([dp(8)])
     texture = ObjectProperty(None, allownone=True)
     texture_size = ListProperty([0, 0])
-    texture_size = ListProperty([0, 0])
+    # Error text to display (for validation feedback)
+    error_text = StringProperty()
